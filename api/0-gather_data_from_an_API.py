d0db2e3a5d1921d8c99d71475cf5f3245c185d9d
Humphrey <humphrey.nyahoja@gmail.com>
2023-11-03 01:56:39 +0000

    my first files

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+def fetch_employee_tasks(employee_id):
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    user_resp = requests.get(user_url)
+    user_data = user_resp.json()
+    employee_name = user_data.get("name", "Unknown")
+
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    todo_response = requests.get(todo_url)
+    todos = todo_response.json()
+
+    total_tasks = len(todos)
+    completed_tasks = [task for task in todos if task.get("completed", False)]
+    number_of_done_tasks = len(completed_tasks)
+
+    print("Employee {} is done with tasks({}/{})".format(employee_name, number_of_done_tasks, total_tasks))
+    for task in completed_tasks:
+        print("\t", task.get("title", "Unknown Task"))
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    fetch_employee_tasks(employee_id)
+
---
+++ api/README.md
@@ -0,0 +1,1 @@
+Application Programming Interface, is a set of rules and protocols that allows one software application to interact with another. It defines the methods and data formats that applications can use to request and exchange information.
