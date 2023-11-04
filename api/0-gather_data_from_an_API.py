77270208cb5d6b800b3d9d906d4a0ca55c5b585e
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-04 22:58:53 +0000

    edited fie 0 again

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,7 +23,7 @@ def fetch_employee_tasks(emp_id):
     completed_tasks = [task for task in todos if task.get("completed", False)]
     number_of_done_tasks = len(completed_tasks)
 
-    print("Employee {} is done with tasks({}/{})".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         employee_name, number_of_done_tasks, total_tasks))
     for task in completed_tasks:
         print("\t", task.get("title"))
