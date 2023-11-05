d522237f86105711514188d7ddf17ae416dc9a7b
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-05 00:29:54 +0000

    added file 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import json
+import requests
+
+def get_json_from_url(url):
+    response = requests.get(url)
+    return response.json()
+
+def fetch_data():
+    users = get_json_from_url("https://jsonplaceholder.typicode.com/users")
+    todos = get_json_from_url("https://jsonplaceholder.typicode.com/todos")
+
+    tasks_by_user = {}
+
+    for todo in todos:
+        user_id = str(todo["userId"])
+        if user_id not in tasks_by_user:
+            user = next(user for user in users if user["id"] == todo["userId"])
+            tasks_by_user[user_id] = []
+
+        task = {
+            "username": user["username"],
+            "task": todo["title"],
+            "completed": todo["completed"]
+        }
+        
+        tasks_by_user[user_id].append(task)
+
+    return tasks_by_user
+
+def main():
+    data = fetch_data()
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+if __name__ == "__main__":
+    main()
